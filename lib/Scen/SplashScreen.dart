fb1b21d1cf51c5fe7f2fdcb1729bf292a76a2ece
Md Rakibull Haque <rakibnirob100@gmail.com>
2023-07-07 19:11:52 +0600

    SliderFinal Add

--- lib/Scen/SplashScreen.dart
+++ lib/Scen/SplashScreen.dart
@@ -13,12 +13,59 @@ class SplashScreen extends StatefulWidget {
   List<PageViewModel> getPages() {
     return [
       PageViewModel(
-        title: "Title of introduction page",
-        body: "Welcome to the app! This is a description of how it works.",
-        image: const Center(
-          child: Icon(Icons.waving_hand, size: 50.0),
-        ),
+          title: "Welcome to the Book Torongo!",
+          body: '''"Immerse yourself in a world of literature with over 400+ books anytime, anywhere."''',
+
+          image:  Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Center(
+              child: Lottie.asset("images/107918-reading-writing-studying-knowledge-books.json"),
+            ),
+          ),
+          decoration: const PageDecoration(
+            titleTextStyle: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFFB71C1C),
+            ),
+            imagePadding: EdgeInsets.only(top: 20),
+          )
       ),
+      PageViewModel(
+          title: "LISTEN TO YOUR BOOK",
+          body: "Explore 200+ Audio Books Available",
+
+        image:  Padding(
+          padding: const EdgeInsets.only(top: 20),
+          child: Center(
+            child: Lottie.asset("images/83451-interactive-animation-music-library-music-menu-buttons.json",height: 180,width: 180,),
+          ),
+        ),
+        decoration: const PageDecoration(
+          titleTextStyle: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.red,
+          ),
+        )
+      ),
+      PageViewModel(
+          title: "Added new book to your library",
+          body: "Discover new books added regularly, keeping your reading experience fresh and exciting",
+
+          image:  Center(
+            child: Lottie.asset("images/72170-books.json"),
+          ),
+          decoration: const PageDecoration(
+            titleTextStyle: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.pink,
+            ),
+            imagePadding: EdgeInsets.only(top: 20),
+          )
+      ),
+
     ];
   }
 
@@ -52,11 +99,12 @@ class _SplashScreenState extends State<SplashScreen> {
             showNextButton: false,
             skip: const Text("Skip"),
             done: const Text("Done"),
+            skipSemantic: "Exit introduction",
             onDone: () {
               Get.offAll(() => LoginPage());
             },
           );
-          Get.to(() => introScreen);
+          Get.offAll(() => introScreen);
         }
       });
     });
--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-      statusBarColor: Colors.deepPurple,
+      statusBarColor: Colors.transparent,
     ));
     return GetMaterialApp(
       debugShowCheckedModeBanner: true,
